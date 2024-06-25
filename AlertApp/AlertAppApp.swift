d0ff314e3ef812e3d29fa8d83a07d6b1c48135ca
Wm023h <111072109+Wm023h@users.noreply.github.com>
2024-06-25 16:42:46 -0500

    Initial Commit

---
+++ AlertApp/AlertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertAppApp.swift
+//  AlertApp
+//
+//  Created by Willow Miller on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AlertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
